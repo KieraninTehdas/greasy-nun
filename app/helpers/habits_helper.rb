59494c58bc1be4a92e95b5bdd1e60dbffc598325
Kieran Porter <kieranintehdas@gmail.com>
2024-12-27 21:50:32 +0000

    Tweak rendering of a habit slightly
    Don't always render habits in a bubble as a link. If looking at a
    specific habit, just render a statement text

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,2 +1,7 @@
 module HabitsHelper
+  def habit_text(habit)
+    start_or_stop = habit.start? ? "start" : "stop"
+
+    "I want to #{start_or_stop} #{habit.name.downcase}"
+  end
 end
--- spec/helpers/habits_helper_spec.rb
+++ spec/helpers/habits_helper_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the HabitsHelper. For example:
-#
-# describe HabitsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe HabitsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#habit_text" do
+    subject { helper.habit_text(habit) }
+
+    context "when habit is good :)" do
+      let(:habit) { Habit.new(name: "Running every day", start: true) }
+
+      it { is_expected.to eq("I want to start running every day") }
+    end
+
+    context "when habit is bad :(" do
+      let(:habit) { Habit.new(name: "Running every day", start: false) }
+
+      it { is_expected.to eq("I want to stop running every day") }
+    end
+  end
 end
