c38e5668469d95c99ae47301206e2b1b091fda80
Kieran Porter <kieranintehdas@gmail.com>
2024-12-28 00:19:43 +0000

    Move tracking point status icon business into the tracking points helper

--- app/helpers/tracking_points_helper.rb
+++ app/helpers/tracking_points_helper.rb
@@ -1,2 +1,13 @@
 module TrackingPointsHelper
+  def status_icon(tracking_point)
+    unless tracking_point.has_data?
+      return "help-circle"
+    end
+
+    if tracking_point.succeeded?
+      "check"
+    else
+      "x"
+    end
+  end
 end
