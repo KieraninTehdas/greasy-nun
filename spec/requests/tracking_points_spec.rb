f8992d5798748dd7703828beeb2f032af44b3d5b
Kieran Porter <kieranintehdas@gmail.com>
2024-12-28 00:10:42 +0000

    Go on, let's commit these tests in case I do decide to look at them

---
+++ spec/requests/tracking_points_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "TrackingPoints", type: :request do
+  describe "GET /new" do
+    it "returns http success" do
+      get "/tracking_points/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/tracking_points/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/tracking_points/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/tracking_points/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
